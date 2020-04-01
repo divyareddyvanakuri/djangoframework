4805989446af04f3e8a920a31de13c9a2995105f
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-04-02 01:04:02 +0530

    [Divya]refactor:refactorted forgorpassword method by adding the functional logic of send mail to resetpassword

--- djangoproject/fundooappnote/views.py
+++ djangoproject/fundooappnote/views.py
@@ -123,6 +123,11 @@ def forgot_password(request):
         domain_name = current_site.domain
         surl = get_surl(str(token))
         z = surl.split("/")
+        mail_subject = "Activate your account"
+        msg1 = render_to_string('password_activation.html', {
+            'username': username, 'domain': domain_name, 'surl': z[2]})
+        send_mail(mail_subject, msg1, EMAIL_HOST_USER,[email], fail_silently=False,)
+        print(msg1)
         return HttpResponse("setpassword through mailed link")    
     else:
         return render(request,'email.html')
